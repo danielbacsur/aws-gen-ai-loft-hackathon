0d5d23f809f6af2ae6febea7c6b3422100da7890
Daniel Bacsur <contact@danielbacsur.com>
2024-09-07 14:59:24 -0700

    feat:add responsive ui for the curriculum generator
    Co-authored-by: Adam Vajda <vjdad4m@gmail.com>

--- apps/oktatas-ai-procedural-curriculum/app/api/generate-curriculum/route.ts
+++ apps/oktatas-ai-procedural-curriculum/app/api/generate-curriculum/route.ts
@@ -12,7 +12,8 @@ export async function POST(req: Request) {
     model: openai("gpt-4o-2024-08-06"),
     system: `Your task is to make a sequential learning experience. Use a combination of all the different types of content to make the learning experience engaging and informative. 
 Always provide a paragraph section before questions, so the user can acquire the knowledge needed to answer the questions. In total you have to make ${numberOfSections} number of sections. 
-You start with n_generations_remaining=${numberOfSections}. Always decrement n_generations_remaining by 1 from the previous section when generating the next section. When n_generations_remaining=0, use an 'end_section'.`,
+The first section you make has to have n_generations_remaining=${numberOfSections}. From that point on decrement n_generations_remaining by 1 from the previous section when generating the next section. 
+When n_generations_remaining=0, use an 'end_section'.`,
     prompt: `The user wants to learn about: ${query}`,
     schema: getCurriculumSchema(numberOfSections),
     maxTokens: numberOfSections * 120,
--- apps/oktatas-ai-procedural-curriculum/app/page.tsx
+++ apps/oktatas-ai-procedural-curriculum/app/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 import { experimental_useObject as useObject } from "ai/react";
 import { getCurriculumSchema } from "@/schemas/curriculum";
+import { CircleThing } from "@/components/circle-thing";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
 
 const NUMBER_OF_SECTIONS = 12;
-
-const tailwindColorsPerSectionType: Record<string, string> = {
-  paragraph_section: "bg-blue-600",
-  short_answer_section: "bg-green-600",
-  multiple_choice_section: "bg-yellow-600",
-  end_section: "bg-red-600",
-};
 
 export default function Page() {
   const { object, submit } = useObject({
@@ -18,31 +16,106 @@ export default function Page() {
     schema: getCurriculumSchema(NUMBER_OF_SECTIONS),
   });
 
+  const [input, setInput] = useState("");
+  const [curriculumProgress, setCurriculumProgress] = useState(0);
+
+  const nextSection = () => {
+    setCurriculumProgress((prev) => Math.min(prev + 1, NUMBER_OF_SECTIONS));
+  };
+
+  const previousSection = () => {
+    setCurriculumProgress((prev) => Math.max(prev - 1, 0));
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    submit({
+      query: input,
+      numberOfSections: NUMBER_OF_SECTIONS,
+    });
+  };
+
   return (
-    <>
-      <button
-        onClick={() =>
-          submit({
-            query: "quantum computing.",
-            numberOfSections: NUMBER_OF_SECTIONS,
-          })
-        }
+    <div className="h-full flex flex-col justify-center">
+      <div
+        className={cn(
+          "overflow-hidden flex-none",
+          object ? "" : "h-0",
+          "transition-all duration-500"
+        )}
       >
-        Generate notifications
-      </button>
-
-      {object?.sections?.map((section, index) => {
-        if (!section) return null;
-
-        return (
-          <div
-            className={tailwindColorsPerSectionType[Object.keys(section)[0]]}
-            key={index}
-          >
-            {JSON.stringify(section)}
+        <div className="container mx-auto px-8 py-12">
+          <div className="w-full h-4 relative">
+            <Progress
+              value={
+                ((object?.sections?.length || 0) / NUMBER_OF_SECTIONS) * 100
+              }
+              outerClassName="absolute top-0 left-0 w-full h-full z-10 bg-gray-200"
+              innerClassName="bg-gray-400"
+            />
+            <Progress
+              value={(curriculumProgress / NUMBER_OF_SECTIONS) * 100}
+              outerClassName="absolute top-0 left-0 w-full h-full z-20 bg-transparent"
+              innerClassName="bg-gray-800"
+            />
           </div>
-        );
-      })}
-    </>
+        </div>
+      </div>
+
+      <main className="h-full flex flex-col justify-center">
+        <div
+          className={cn(
+            "overflow-hidden flex-none text-center",
+            object ? "h-0" : "pb-8",
+            "transition-all duration-500"
+          )}
+        >
+          <h1>What do you want to learn today?</h1>
+        </div>
+        <div
+          className={cn(
+            "overflow-hidden grid place-items-center",
+            object ? "flex-grow" : "flex-none h-0",
+            "transition-all duration-500"
+          )}
+        >
+          <div className="container mx-auto px-8 py-4 h-full flex flex-col justify-between">
+            <div>
+              {JSON.stringify(object?.sections?.[curriculumProgress], null, 2)}
+            </div>
+            <div className="flex justify-between">
+              <button onClick={previousSection}>Previous</button>
+              <button onClick={nextSection}>Next</button>
+            </div>
+          </div>
+        </div>
+        <div className={cn("flex-none h-32 text-center")}>
+          <div className="container mx-auto px-8 py-4 animate-pulse">
+            <form onSubmit={onSubmit} className="flex space-x-4 relative">
+              <Input
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                className="rounded-full h-14 backdrop-filter backdrop-blur-md bg-white/80 shadow-lg"
+              />
+              <button
+                className="absolute right-4 top-1/2 transform -translate-y-1/2"
+                type="submit"
+              >
+                Learn
+              </button>
+            </form>
+          </div>
+        </div>
+      </main>
+
+      <div
+        className={cn(
+          "absolute top-0 left-0 right-0 bottom-0 -z-10 pointer-events-none",
+          object ? "opacity-0" : "opacity-100"
+        )}
+      >
+        <CircleThing />
+      </div>
+    </div>
   );
 }
